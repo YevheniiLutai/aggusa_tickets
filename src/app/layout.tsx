3e24e6935e17c80c5dc06c5f5dc5837726bce35d
Yevhenii Lutai <yevheniilutai@Yevheniis-MacBook-Air.local>
2025-08-16 19:45:21 -0700

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,26 @@ import { ReactNode } from 'react'
 export const metadata = {
   title: 'AGG USA Tickets',
   description: 'AGG USA Tickets — Official Ticketing Platform for Aesthetic Group Gymnastics Events',
- image: 'banner.jpg'
+  openGraph: {
+    title: 'AGG USA Tickets',
+    description: 'AGG USA Tickets — Official Ticketing Platform for Aesthetic Group Gymnastics Events',
+    url: 'https://www.tickets.aggusafederation.com/',
+    images: [
+      {
+        url: '/banner.jpg',
+        width: 600,
+        height: 315,
+        alt: 'AGG USA Banner',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AGG USA Tickets',
+    description: 'AGG USA Tickets — Official Ticketing Platform for Aesthetic Group Gymnastics Events',
+    images: ['/banner.jpg'],
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
